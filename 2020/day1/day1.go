8f05662b4ce5321ea9d56dd22ded7fb16d2868e6
Dean Wan <deanwan14@gmail.com>
2024-05-14 13:15:52 +1000

    feat: add solution for day1, part1

--- 2020/day1/day1.go
+++ 2020/day1/day1.go
@@ -1,37 +1,69 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 var (
-	target = 2020
+	target     = 2020
+	stringNums = []string{}
 )
 
 func main() {
-	fileLocation := flag.String("inputLocation", "input.txt", "define a file location for thje input file")
+	fileLocation := flag.String("inputLocation", "input.txt", "define a file location for the input file")
 	flag.Parse()
 
-	file, err := os.Open(*fileLocation)
+	file, err := os.ReadFile(*fileLocation)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+	fileContent := string(file)
+	stringNums = strings.Split(fileContent, "\n")
+	intNums := convertStringToIntSlice(stringNums)
+
+	startIdx, endIdx, found := parseNumbers(intNums)
+	if !found {
+		log.Fatalf("Could not find numbers that sum to %d", target)
 	}
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println(intNums[startIdx] * intNums[endIdx])
 }
 
-func sumTo(a, b int) bool {
+func sumToTarget(a, b int) bool {
 	return a+b == target
+}
+
+func parseNumbers(numbers []int) (int, int, bool) {
+	for startIdx := 0; startIdx < len(numbers)-1; startIdx++ {
+		for endIdx := startIdx + 1; endIdx < len(numbers); endIdx++ {
+			if sumToTarget(numbers[startIdx], numbers[endIdx]) {
+				return startIdx, endIdx, true
+			}
+		}
+	}
+	return 0, 0, false
+}
+
+func convertStringToIntSlice(nums []string) []int {
+	intSlice := make([]int, len(nums))
+
+	for i, element := range nums {
+		if len(element) < 1 {
+			continue
+		}
+
+		num, err := strconv.Atoi(element)
+		if err != nil {
+			log.Fatal(err)
+		}
+		intSlice[i] = num
+	}
+
+	return intSlice
 }
