db208e3832c6ea5104bb889c15d502b73fd429ac
Dean Wan <deanwan14@gmail.com>
2024-05-14 11:43:15 +1000

    feat: add sum function

--- 2020/day1/day1.go
+++ 2020/day1/day1.go
@@ -1,1 +1,19 @@
 package main
+
+var (
+	target = 2020
+)
+
+func main() {
+	// Load file line by line for efficiency
+	// Likely need type conversion
+	// Check if sums add up to 2020
+	// keep track of ints that sum to 2020
+	// once 2 ints are found, terminate file loading
+	// multiply 2 ints
+	// return result
+}
+
+func sumTo(a, b int) bool {
+	return a+b == target
+}
--- 2020/day1/day1_test.go
+++ 2020/day1/day1_test.go
@@ -1,1 +1,26 @@
-package main_test
+package main
+
+import (
+	"testing"
+)
+
+func TestSumTo(t *testing.T) {
+	data := []struct {
+		testCase string
+		target   int
+		operand1 int
+		operand2 int
+		want     bool
+	}{
+		{"target correct", 5, 3, 2, true},
+		{"target incorrect", 6, 3, 2, false},
+	}
+
+	for _, d := range data {
+		target = d.target
+		got := sumTo(d.operand1, d.operand2)
+		if got != d.want {
+			t.Errorf("sumTo() - got: %t, expected: %t", got, d.want)
+		}
+	}
+}
