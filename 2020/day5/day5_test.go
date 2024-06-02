069ed275d38aa714077d3559f95bf78aa867de55
Dean Wan <deanwan14@gmail.com>
2024-06-02 21:23:12 +1000

    test: set up test scaffold

--- 2020/day5/day5_test.go
+++ 2020/day5/day5_test.go
@@ -3,9 +3,33 @@ package main
 import "testing"
 
 func Test_recalculateUpperBound(t *testing.T) {
+	tests := map[string]struct {
+		input *boundary
+		want  *boundary
+	}{}
 
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := recalculateUpperBound(test.input)
+			if got != test.want {
+				t.Errorf("recalculateUpperBound %s err: got: %+v, want: %+v", name, got, test.want)
+			}
+		})
+	}
 }
 
 func Test_recalculateLowerBound(t *testing.T) {
+	tests := map[string]struct {
+		input *boundary
+		want  *boundary
+	}{}
 
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := recalculateLowerBound(test.input)
+			if got != test.want {
+				t.Errorf("recalculateLowerBound %s err: got: %+v, want: %+v", name, got, test.want)
+			}
+		})
+	}
 }
